ce4dbdae02cf2f8b98111dab90484a860b3f10d0
Mark Stabler <mark@markstabler.com>
2020-03-13 12:07:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,37 +3,7 @@ class Book
     @title = title
   end
 
-  def title=(title)
-    @title = title
-  end
-
-  def title
-    @title
-  end
-
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
+  attr_accessor :title, :author, :page_count, :genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
