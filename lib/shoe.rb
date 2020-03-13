4204cf656614911aa6649f3dd3a9469bb9d9e8c1
Mark Stabler <mark@markstabler.com>
2020-03-13 12:18:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,9 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color :size :material :condition
+  # attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+
+end
